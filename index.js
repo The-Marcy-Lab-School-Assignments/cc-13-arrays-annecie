37e7c25fbd491ed121542d9601fbc8121d590c45
annecie <ajbexcelsior@gmail.com>
2021-11-08 15:13:00 +0000

    code challenge complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//write your code here
+function averageInArray(arr){
+    let average = 0;
+    for ( let i = 0; i < arr.length; i++) {
+        average += arr[i];
+    }
+    return average / arr.length
+}
+
+averageInArray([2,4,5,7,8]);
